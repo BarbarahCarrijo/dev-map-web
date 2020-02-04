6058013117cc437cf1f88d63978c6ee3ac49a179
Barbara Carrijo <barbarahcarrijo@gmail.com>
2020-02-03 22:46:46 -0300

    adds state of variables within the form and indentation app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import './global.css'
 import './App.css'
@@ -6,83 +6,139 @@ import './Sidebar.css'
 import './Main.css'
 
 function App() {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
 
-    return (
-        <div id="app">
-            <aside>
-                <strong>Cadastrar</strong>
-                <form>
-                    <div className="input-block">
-                        <label htmlFor="github_username">Usuário do Github</label>
-                        <input name="github_username" id="username_github" required/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="techs">Tecnologias</label>
-                        <input name="techs" id="techs" required/>
-                    </div>
-                    <div className="input-group">
-                        <div className="input-block">
-                            <label htmlFor="latitude">Latitude</label>
-                            <input name="latitude" id="latitude" required/>
-                        </div>
-                        <div className="input-block">
-                            <label htmlFor="longitude">Longitude</label>
-                            <input name="longitude" id="longitude" required/>
-                        </div>
-                    </div>
-                    <button type="submit">Salvar</button>
-                </form>
-            </aside>
-            <main>
-                <ul>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars2.githubusercontent.com/u/19477838?s=460&v=4" alt="Bárbara Carrijo"/>
-                            <div className="user-info">
-                                <strong>Bárbara Carrijo</strong>
-                                <span>Ruby, Rails, React</span>
-                            </div>
-                        </header>
-                        <p>Ruby on Rails developer at @Siedos - JavaScript, Java and C# enthusiast - Internet Systems Technology Student</p>
-                        <a href="https://github.com/BarbarahCarrijo">Acessar perfil no github</a>
-                    </li>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars2.githubusercontent.com/u/19477838?s=460&v=4" alt="Bárbara Carrijo"/>
-                            <div className="user-info">
-                                <strong>Bárbara Carrijo</strong>
-                                <span>Ruby, Rails, React</span>
-                            </div>
-                        </header>
-                        <p>Ruby on Rails developer at @Siedos - JavaScript, Java and C# enthusiast - Internet Systems Technology Student</p>
-                        <a href="https://github.com/BarbarahCarrijo">Acessar perfil no github</a>
-                    </li>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars2.githubusercontent.com/u/19477838?s=460&v=4" alt="Bárbara Carrijo"/>
-                            <div className="user-info">
-                                <strong>Bárbara Carrijo</strong>
-                                <span>Ruby, Rails, React</span>
-                            </div>
-                        </header>
-                        <p>Ruby on Rails developer at @Siedos - JavaScript, Java and C# enthusiast - Internet Systems Technology Student</p>
-                        <a href="https://github.com/BarbarahCarrijo">Acessar perfil no github</a>
-                    </li>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars2.githubusercontent.com/u/19477838?s=460&v=4" alt="Bárbara Carrijo"/>
-                            <div className="user-info">
-                                <strong>Bárbara Carrijo</strong>
-                                <span>Ruby, Rails, React</span>
-                            </div>
-                        </header>
-                        <p>Ruby on Rails developer at @Siedos - JavaScript, Java and C# enthusiast - Internet Systems Technology Student</p>
-                        <a href="https://github.com/BarbarahCarrijo">Acessar perfil no github</a>
-                    </li>
-                </ul>
-            </main>
-        </div>
-    );
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const {latitude, longitude} = position.coords
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input
+              name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+            />
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
+          </div>
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input
+                type="number"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/19477838?s=460&v=4"
+                   alt="Bárbara Carrijo"/>
+              <div className="user-info">
+                <strong>Bárbara Carrijo</strong>
+                <span>Ruby, Rails, React</span>
+              </div>
+            </header>
+            <p>Ruby on Rails developer at @Siedos - JavaScript, Java and C# enthusiast - Internet Systems
+              Technology Student</p>
+            <a href="https://github.com/BarbarahCarrijo">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/19477838?s=460&v=4"
+                   alt="Bárbara Carrijo"/>
+              <div className="user-info">
+                <strong>Bárbara Carrijo</strong>
+                <span>Ruby, Rails, React</span>
+              </div>
+            </header>
+            <p>Ruby on Rails developer at @Siedos - JavaScript, Java and C# enthusiast - Internet Systems
+              Technology Student</p>
+            <a href="https://github.com/BarbarahCarrijo">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/19477838?s=460&v=4"
+                   alt="Bárbara Carrijo"/>
+              <div className="user-info">
+                <strong>Bárbara Carrijo</strong>
+                <span>Ruby, Rails, React</span>
+              </div>
+            </header>
+            <p>Ruby on Rails developer at @Siedos - JavaScript, Java and C# enthusiast - Internet Systems
+              Technology Student</p>
+            <a href="https://github.com/BarbarahCarrijo">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/19477838?s=460&v=4"
+                   alt="Bárbara Carrijo"/>
+              <div className="user-info">
+                <strong>Bárbara Carrijo</strong>
+                <span>Ruby, Rails, React</span>
+              </div>
+            </header>
+            <p>Ruby on Rails developer at @Siedos - JavaScript, Java and C# enthusiast - Internet Systems
+              Technology Student</p>
+            <a href="https://github.com/BarbarahCarrijo">Acessar perfil no github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
 }
 
 export default App;
