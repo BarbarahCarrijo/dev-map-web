f8bc21da25b3c6371fe00ef6c5ce6e4f0b32cdbc
Barbara Carrijo <barbarahcarrijo@gmail.com>
2020-02-04 23:33:01 -0300

    fragment components and add function for listing devs

--- src/App.js
+++ src/App.js
@@ -6,150 +6,39 @@ import './App.css'
 import './Sidebar.css'
 import './Main.css'
 
-function App() {
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
 
-  const [github_username, setGithubUsername] = useState('');
-  const [techs, setTechs] = useState('');
+function App() {
+  const [devs, setDevs] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const {latitude, longitude} = position.coords
+    async function loadDevs() {
+      const res = await api.get('/devs');
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000,
-      }
-    )
+      setDevs(res.data);
+    }
+
+    loadDevs();
   }, []);
 
-  async function handleAddDev(e) {
-    e.preventDefault();
+  async function handleAddDev(data) {
+    const res = await api.post('/devs', data);
 
-    const res = await api.post('/devs', {
-      github_username,
-      techs,
-      latitude,
-      longitude
-    });
-
-    setGithubUsername('');
-    setTechs('');
+    setDevs([...devs, res.data]);
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input
-              name="github_username"
-              id="github_username"
-              required
-              value={github_username}
-              onChange={e => setGithubUsername(e.target.value)}
-            />
-          </div>
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input
-              name="techs"
-              id="techs"
-              required
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                name="latitude"
-                id="latitude"
-                required
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                name="longitude"
-                id="longitude"
-                required
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/19477838?s=460&v=4"
-                   alt="Bárbara Carrijo"/>
-              <div className="user-info">
-                <strong>Bárbara Carrijo</strong>
-                <span>Ruby, Rails, React</span>
-              </div>
-            </header>
-            <p>Ruby on Rails developer at @Siedos - JavaScript, Java and C# enthusiast - Internet Systems
-              Technology Student</p>
-            <a href="https://github.com/BarbarahCarrijo">Acessar perfil no github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/19477838?s=460&v=4"
-                   alt="Bárbara Carrijo"/>
-              <div className="user-info">
-                <strong>Bárbara Carrijo</strong>
-                <span>Ruby, Rails, React</span>
-              </div>
-            </header>
-            <p>Ruby on Rails developer at @Siedos - JavaScript, Java and C# enthusiast - Internet Systems
-              Technology Student</p>
-            <a href="https://github.com/BarbarahCarrijo">Acessar perfil no github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/19477838?s=460&v=4"
-                   alt="Bárbara Carrijo"/>
-              <div className="user-info">
-                <strong>Bárbara Carrijo</strong>
-                <span>Ruby, Rails, React</span>
-              </div>
-            </header>
-            <p>Ruby on Rails developer at @Siedos - JavaScript, Java and C# enthusiast - Internet Systems
-              Technology Student</p>
-            <a href="https://github.com/BarbarahCarrijo">Acessar perfil no github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/19477838?s=460&v=4"
-                   alt="Bárbara Carrijo"/>
-              <div className="user-info">
-                <strong>Bárbara Carrijo</strong>
-                <span>Ruby, Rails, React</span>
-              </div>
-            </header>
-            <p>Ruby on Rails developer at @Siedos - JavaScript, Java and C# enthusiast - Internet Systems
-              Technology Student</p>
-            <a href="https://github.com/BarbarahCarrijo">Acessar perfil no github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
         </ul>
       </main>
     </div>
